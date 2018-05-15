cfd58804c517b6d0c0306a3d6903d354f6ffe6c5
hkim <muncok@dal.snu.ac.kr>
2018-05-15 21:25:23 +0900

    optimize sv_score indexing, fix the TDNN

--- sd_model_train/sd_tdnn_train.py
+++ sd_model_train/sd_tdnn_train.py
@@ -1,51 +1,92 @@
 # coding: utf-8
+import os
+
 import pandas as pd
+import argparse
 
-from ..sv_system.utils.parser import test_config
-from ..sv_system.model.TDNN import TdnnModel
-from ..sv_system.data.dataset import SpeechDataset
-from ..sv_system.utils import secToSample, secToFrames
-from ..sv_system.utils.data_utils import split_df
+from sv_system.data import dataloader as dloader
+from sv_system.data.dataset import SpeechDataset
+from sv_system.model.TDNN import TdnnModel
+from sv_system.train import si_train
+from sv_system.utils import secToSample, secToFrames
+from sv_system.utils.data_utils import split_df
+from sv_system.utils.parser import test_config
 
-from ..sv_system.train import si_train
-from ..sv_system.data import dataloader as dloader
+#########################################
+# Parser
+#########################################
+parser = argparse.ArgumentParser()
+parser.add_argument('-nep', '--epochs',
+                    type=int,
+                    help='number of epochs to train for',
+                    default=140)
 
+parser.add_argument('-dataset',
+                    type=str,
+                    help='dataset',
+                    default='voxc')
 
+parser.add_argument('-inLen', '--input_length',
+                    type=int,
+                    help='length of input audio, sec',
+                    default=3)
+
+parser.add_argument('-spLen', '--splice_length',
+                    type=float,
+                    help='length of spliced audio snippet, sec',
+                    default=0.2)
 
 #########################################
 # Configuration
 #########################################
-model = "Tdnn"
+args = parser.parse_args()
+model = "TDNN"
+dataset = args.dataset
 si_config = test_config(model)
 
-#########################################
-# Input configure
-#########################################
 si_config['input_clip'] = True
-sec_input = 3
+sec_input = args.input_length
+sec_splice = args.splice_length
 si_config['input_length'] = secToSample(sec_input)
 si_config['input_frames'] = secToFrames(sec_input)
-si_config['splice_frames'] = secToFrames(0.1)
+si_config['splice_frames'] = secToFrames(sec_splice)
 si_config['input_format'] = 'fbank'
+
+si_train.set_seed(si_config)
+si_config['n_epochs'] = args.epochs
+si_config['print_step'] = 100
+si_config['lr'] = [0.001, 0.0001]
+si_config['schedule'] = [20]
+
+if dataset == "voxc":
+    si_config['data_folder'] = "../dataset/voxceleb/"
+    df = pd.read_pickle("../dataset/dataframes/Voxc_Dataframe.pkl")
+    n_labels = 1260
+elif dataset == "reddots":
+    si_config['data_folder'] = "../dataset/reddots_r2015q4_v1/wav"
+    df = pd.read_pickle("../dataset/dataframes/Reddots/Reddots_Dataframe.pkl")
+    n_labels = 70
+else:
+    raise FileNotFoundError
+
+si_config['output_file'] = "../models/compare_train_methods/" \
+                           "{dset}/si_{dset}_{model}_{in_len}s_" \
+                           "{s_len}s_{in_format}.pt".format(dset=dataset, in_len=sec_input,
+                                                            s_len=sec_splice,
+                                                            in_format=si_config['input_format'],
+                                                            model=model)
 
 #########################################
 # Model Initialization
 #########################################
-si_model = TdnnModel(si_config, 1260)
-# si_config['output_file'] = "models/reddots/si_train/si_reddots_tdnnfc_3s_0.1s_mean.pt"
-si_config['output_file'] = "models/voxc/si_train/si_voxc_tdnnfc_3s_0.1s_mean.pt"
-# si_model.load(si_config['output_file'])
+si_model = TdnnModel(si_config, n_labels)
+if os.path.isfile(si_config['output_file']):
+    si_model.load(si_config['output_file'])
+
 #########################################
 # Model Training
 #########################################
-si_train.set_seed(si_config)
-si_config['n_epochs'] = 100
-si_config['print_step'] = 200
-si_config['data_folder'] = "dataset/voxceleb/"
-# si_config['data_folder'] = "dataset/reddots_r2015q4_v1/wav"
 
-df = pd.read_pickle("dataset/dataframes/Voxc_Dataframe.pkl")
-# df = pd.read_pickle("dataset/dataframes/Reddots/Reddots_Dataframe.pkl")
 split_dfs = split_df(df)
 loaders = []
 for i, df in enumerate(split_dfs):
