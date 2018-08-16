eb8564426beaaaa923a672813cbce9d7c11c3953
hkim <muncok@dal.snu.ac.kr>
2018-08-16 19:13:27 +0900

    remove unneccesary files

--- run_plda_gcommand.sh
+++ run_plda_gcommand.sh
@@ -73,33 +73,3 @@ echo "minDCF(p-target=0.001): $mindcf2"
 
 fi
 
-# xvector version
-#if [ $stage -le 10 ]; then
-  ## Compute the mean vector for centering the evaluation xvectors.
-  #$train_cmd $nnet_dir/xvectors_train/log/compute_mean.log \
-    #ivector-mean scp:$nnet_dir/xvectors_train/xvector.scp \
-    #$nnet_dir/xvectors_train/mean.vec || exit 1;
-
-  ## This script uses LDA to decrease the dimensionality prior to PLDA.
-  #lda_dim=200
-  #$train_cmd $nnet_dir/xvectors_train/log/lda.log \
-    #ivector-compute-lda --total-covariance-factor=0.0 --dim=$lda_dim \
-    #"ark:ivector-subtract-global-mean scp:$nnet_dir/xvectors_train/xvector.scp ark:- |" \
-    #ark:data/train/utt2spk $nnet_dir/xvectors_train/transform.mat || exit 1;
-
-  ## Train the PLDA model.
-  #$train_cmd $nnet_dir/xvectors_train/log/plda.log \
-    #ivector-compute-plda ark:data/train/spk2utt \
-    #"ark:ivector-subtract-global-mean scp:$nnet_dir/xvectors_train/xvector.scp ark:- | transform-vec $nnet_dir/xvectors_train/transform.mat ark:- ark:- | ivector-normalize-length ark:-  ark:- |" \
-    #$nnet_dir/xvectors_train/plda || exit 1;
-#fi
-
-#if [ $stage -le 11 ]; then
-  #$train_cmd exp/scores/log/voxceleb1_test_scoring.log \
-    #ivector-plda-scoring --normalize-length=true \
-    #"ivector-copy-plda --smoothing=0.0 $nnet_dir/xvectors_train/plda - |" \
-    #"ark:ivector-subtract-global-mean $nnet_dir/xvectors_train/mean.vec scp:$nnet_dir/xvectors_voxceleb1_test/xvector.scp ark:- | transform-vec $nnet_dir/xvectors_train/transform.mat ark:- ark:- | ivector-normalize-length ark:- ark:- |" \
-    #"ark:ivector-subtract-global-mean $nnet_dir/xvectors_train/mean.vec scp:$nnet_dir/xvectors_voxceleb1_test/xvector.scp ark:- | transform-vec $nnet_dir/xvectors_train/transform.mat ark:- ark:- | ivector-normalize-length ark:- ark:- |" \
-    #"cat '$voxceleb1_trials' | cut -d\  --fields=1,2 |" exp/scores_voxceleb1_test || exit 1;
-#fi
-
